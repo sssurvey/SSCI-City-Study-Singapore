40df43895ea88e738d9b5239225345dffcaa6447
haominshi <haominshi@gmail.com>
2018-04-09 18:01:01 -0500

    update

--- MainConsole.py
+++ MainConsole.py
@@ -7,7 +7,7 @@ import sys
 
 ############
 
-wb1 = openpyxl.load_workbook('data/workBook1.xlsx')
+wb1 = openpyxl.load_workbook('data/workBook1.xlsx')  # load a1.xls A1- Resident Population by Age Group, Ethnic Group, Sex and Residential Status.xls
 sheet1 = wb1['Sheet1']
 
 # processing Sheets
@@ -28,7 +28,7 @@ def process1(sheetT):
         print("!!!FEMALE!!! data below")
         for x in range(4, 22):
             percentage = float((sheetT.cell(row=x, column=4).value)/totalPopulation)
-            printf('age group is: %s, percentage of female in total population is: %f \n', sheetT.cell(row=x, column=1).value,percentage)
+            printf('age group is: %s, percentage of female in total population is: %f \n', sheetT.cell(row=x, column=1).value, percentage)
 
         # in singapore the age for retirement is 62
         # thus the male labor pool is roughly this
@@ -44,18 +44,29 @@ def process1(sheetT):
         print("-----------------------------------------------------------------")
         printf("Female workforce total's percentage in total population is: %f\n", total_female_workforce_percentage)
 
+        femaleOverTotalPop = sheetT.cell(row=3, column=3).value / totalPopulation
+        maleOverTotalPop = 1-femaleOverTotalPop
+        printf("just for the information: female percentage in total population is: %f And for male is: %f \n", femaleOverTotalPop, maleOverTotalPop)
 
 
+# print helper
 def printf(format, *args):
     sys.stdout.write(format % args)
 
-def main():
+def main(): # main console
     print("Welcome to Console\n")
-
-    # load a1.xls A1- Resident Population by Age Group, Ethnic Group, Sex and Residential Status.xls
-
     print("in work book 1 you have: ")
-    print(*wb1, sep=", ")
-    process1(sheet1)
 
+    print(*wb1, sep=", ")
+    while True:
+        print("type in 1 to start")
+        print("type in exit to quit")
+        usrIn = input(">>>Type in Command $ ")
+        if usrIn == "exit":
+            break
+        if usrIn == "1":
+            process1(sheet1)
+
+
+##########################################################################
 main()
