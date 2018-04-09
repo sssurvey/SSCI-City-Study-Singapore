094ef2bb209e588cf1171e8fb5b0b8753f3e5a01
haominshi <haominshi@gmail.com>
2018-04-09 17:22:08 -0500

    update

--- MainConsole.py
+++ MainConsole.py
@@ -3,17 +3,41 @@
 
 # imports
 import openpyxl
+import sys
 
 ############
 
-print("Welcome to Console\n")
-
-# load a1.xls A1- Resident Population by Age Group, Ethnic Group, Sex and Residential Status.xls
 wb1 = openpyxl.load_workbook('data/workBook1.xlsx')
-print("in work book 1 you have: ")
-print(*wb1, sep=", ")
-
 sheet1 = wb1['Sheet1']
 
-for x in range(4, 22):
-    print(sheet1.cell(row = x,column=1).value)
+# processing Sheets
+# basic percentage of population
+def process1(sheetT):
+    print(type(sheetT))
+    totalPopulation = sheetT.cell(row=3,column=2).value
+
+    print("!!!MALE!!! data below")
+    for x in range(4, 22):
+        percentage = float((sheetT.cell(row=x,column=3).value)/totalPopulation)
+        printf('age group is: %s, percentage of male in total population is: %f \n', sheetT.cell(row=x, column=1).value, percentage)
+    print("-----------------------------------------------------------------")
+    print("!!!FEMALE!!! data below")
+    for x in range(4, 22):
+        percentage = float((sheetT.cell(row=x,column=4).value)/totalPopulation)
+        printf('age group is: %s, percentage of female in total population is: %f \n', sheetT.cell(row=x, column=1).value,percentage)
+
+
+
+def printf(format, *args):
+    sys.stdout.write(format % args)
+
+def main():
+    print("Welcome to Console\n")
+
+    # load a1.xls A1- Resident Population by Age Group, Ethnic Group, Sex and Residential Status.xls
+
+    print("in work book 1 you have: ")
+    print(*wb1, sep=", ")
+    process1(sheet1)
+
+main()
