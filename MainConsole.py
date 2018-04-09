cbb02a86fd06b210fd197738511b9c56921798cc
haominshi <haominshi@gmail.com>
2018-04-09 17:43:06 -0500

    update

--- MainConsole.py
+++ MainConsole.py
@@ -13,18 +13,36 @@ sheet1 = wb1['Sheet1']
 # processing Sheets
 # basic percentage of population
 def process1(sheetT):
-    print(type(sheetT))
-    totalPopulation = sheetT.cell(row=3,column=2).value
 
-    print("!!!MALE!!! data below")
-    for x in range(4, 22):
-        percentage = float((sheetT.cell(row=x,column=3).value)/totalPopulation)
-        printf('age group is: %s, percentage of male in total population is: %f \n', sheetT.cell(row=x, column=1).value, percentage)
-    print("-----------------------------------------------------------------")
-    print("!!!FEMALE!!! data below")
-    for x in range(4, 22):
-        percentage = float((sheetT.cell(row=x,column=4).value)/totalPopulation)
-        printf('age group is: %s, percentage of female in total population is: %f \n', sheetT.cell(row=x, column=1).value,percentage)
+    if sheetT == sheet1:
+        print(type(sheetT))
+        totalPopulation = sheetT.cell(row=3,column=2).value
+        male_workforce_total = 0
+        female_workforce_total = 0
+
+        print("!!!MALE!!! data below")
+        for x in range(4, 22):
+            percentage = float((sheetT.cell(row=x, column=3).value)/totalPopulation)
+            printf('age group is: %s, percentage of male in total population is: %f \n', sheetT.cell(row=x, column=1).value, percentage)
+        print("-----------------------------------------------------------------")
+        print("!!!FEMALE!!! data below")
+        for x in range(4, 22):
+            percentage = float((sheetT.cell(row=x, column=4).value)/totalPopulation)
+            printf('age group is: %s, percentage of female in total population is: %f \n', sheetT.cell(row=x, column=1).value,percentage)
+
+        # in singapore the age for retirement is 62
+        # thus the male labor pool is roughly this
+        for x in range(7,22-6): # x need to start form 7, ends at 22-6
+            male_workforce_total += sheetT.cell(row=x, column=3).value
+        for x in range(7,22-6): # x need to start form 7, ends at 22-6
+            female_workforce_total += sheetT.cell(row=x, column=4).value
+
+        total_male_workforce_percentage = male_workforce_total/totalPopulation
+        total_female_workforce_percentage = female_workforce_total / totalPopulation
+        print("-----------------------------------------------------------------")
+        printf("Male workforce total's percentage in total population is: %f \n", total_male_workforce_percentage)
+        print("-----------------------------------------------------------------")
+        printf("Female workforce total's percentage in total population is: %f\n", total_female_workforce_percentage)
 
 
 
