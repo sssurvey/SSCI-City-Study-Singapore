fce4208f01cf64aaa6bc3a653b1a58bdb87bc23a
haominshi <haominshi@gmail.com>
2018-04-09 15:25:21 -0500

    update

--- MainConsole.py
+++ MainConsole.py
@@ -7,3 +7,13 @@ import openpyxl
 ############
 
 print("Welcome to Console\n")
+
+# load a1.xls A1- Resident Population by Age Group, Ethnic Group, Sex and Residential Status.xls
+wb1 = openpyxl.load_workbook('data/workBook1.xlsx')
+print("in work book 1 you have: ")
+print(*wb1, sep=", ")
+
+sheet1 = wb1['Sheet1']
+
+for x in range(4, 22):
+    print(sheet1.cell(row = x,column=1).value)
